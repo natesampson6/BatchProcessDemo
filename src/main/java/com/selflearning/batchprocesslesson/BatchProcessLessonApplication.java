6df0c38236d5538fe251128f8105818953ff4ca1
natesampson6 <35074123+natesampson6@users.noreply.github.com>
2020-07-09 21:38:27 -0500

    Initial Commit
    Simple batch processing application that accepts a list of lowercased names in a .csv file, transforms them into uppercase, writes them to a SQL DB, and prints the result in the console

---
+++ src/main/java/com/selflearning/batchprocesslesson/BatchProcessLessonApplication.java
@@ -0,0 +1,13 @@
+package com.selflearning.batchprocesslesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchProcessLessonApplication {
+
+	public static void main(String[] args) {
+		System.exit (SpringApplication.exit (SpringApplication.run(BatchProcessLessonApplication.class, args)));
+	}
+
+}
---
+++ src/test/java/com/selflearning/batchprocesslesson/BatchProcessLessonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selflearning.batchprocesslesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchProcessLessonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
